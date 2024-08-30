206ec7ea6c82128baa8ba4ab1ad01ae21acc7736
zeshanziya <zeshan.ziya@axelerant.com>
2024-08-30 11:14:26 +0000

    add project details card

---
+++ plugins/platformsh/src/components/Cards/ProjectDetailsCard/ProjectDetailsCard.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ProjectDetailsCard } from './ProjectDetailsCard';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { screen } from '@testing-library/react';
+import {
+  registerMswTestHooks,
+  renderInTestApp,
+  TestApiProvider,
+} from '@backstage/test-utils';
+import { platformshApiRef } from '../../../api';
+
+describe('ProjectDetailsCard', () => {
+  const server = setupServer();
+  // Enable sane handlers for network requests
+  registerMswTestHooks(server);
+
+  const platformshApi: jest.Mocked<typeof platformshApiRef.T> = {
+    listProjects: jest.fn(),
+  };
+
+  const Wrapper = ({ children }: { children?: React.ReactNode }) => (
+    <TestApiProvider apis={[[platformshApiRef, platformshApi]]}>
+      {children}
+    </TestApiProvider>
+  );
+
+  // setup mock response
+  beforeEach(() => {
+    server.use(
+      rest.get('/*', (_, res, ctx) => res(ctx.status(200), ctx.json({}))),
+    );
+  });
+
+  it('should render', async () => {
+    await renderInTestApp(
+      <Wrapper>
+        <ProjectDetailsCard projectId='abc-123'/>
+      </Wrapper>,
+    );
+    expect(
+      screen.getByText('Project Details'),
+    ).toBeInTheDocument();
+  });
+});
---
+++ plugins/platformsh/src/components/Cards/ProjectDetailsCard/ProjectDetailsCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { InfoCard } from '@backstage/core-components';
+
+export const ProjectDetailsCard = ({ projectId }: {projectId: string}) => {
+  return (
+    <InfoCard title="Project Details">
+      This is project details. { projectId }
+    </InfoCard>
+  );
+};
