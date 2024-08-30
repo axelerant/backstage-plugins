6133085cfc376b1deb9506f4505ec8a643b9a490
zeshanziya <zeshan.ziya@axelerant.com>
2024-08-30 11:18:32 +0000

    add ProjectDetails card to entity tab component
    Signed-off-by: zeshanziya <zeshan.ziya@axelerant.com>

--- plugins/platformsh/src/components/EntityTabComponent/EntityTabComponent.tsx
+++ plugins/platformsh/src/components/EntityTabComponent/EntityTabComponent.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { ProjectDetailsCard } from '../Cards/ProjectDetailsCard';
+import { useEntity } from '@backstage/plugin-catalog-react';
+import { ANNOTATION_PLATFORMSH_PROJECT } from '../../utils';
 
-export const EntityTabComponent = () => (
-  <Grid container spacing={3} direction="column">
-    <Grid item>This is Platformsh entity tab</Grid>
-  </Grid>
-);
+export const EntityTabComponent = () => {
+  const { entity } = useEntity();
+  const projectId =
+    entity.metadata.annotations?.[ANNOTATION_PLATFORMSH_PROJECT] ?? '';
+  return (
+    <Grid container spacing={3} direction="column">
+      <Grid item>This is Platformsh entity tab</Grid>
+      <Grid>
+        <ProjectDetailsCard projectId={projectId} />
+      </Grid>
+    </Grid>
+  );
+};
